19a72d9db23d75127650ddd00e4a5ec54bbc7159
Angel Dela Rosa <akmdelarosa@gmail.com>
2016-05-06 01:19:27 -0400

    updated db config

--- server/mysqlConnectionString.js
+++ server/mysqlConnectionString.js
@@ -17,10 +17,10 @@ var mysqlConnectionString = {
     },
     
     prod : {
-      host: 'localhost',
-      user: 'akmdelarosa',
-      password : '',
-      database : 'c9'
+      host: 'us-cdbr-iron-east-03.cleardb.net',
+      user: 'b48c8a0d0f1b10',
+      password : 'ad01b514',
+      database : 'heroku_6cecfd8d370547f'
     }
     
   }
